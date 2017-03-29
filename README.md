16214bce9db11028c58e035b32638a76160fd36d
liuxueyang <zypz457@sina.com>
2017-03-29 22:06:22 +0800

    Refactor and optimize code. Fix some bugs. Update README.md

--- README.md
+++ README.md
@@ -1,28 +1,52 @@
-# DownloadWallpaper
-Download wallpapers from http://wallpaperscraft.com/
+下载壁纸
+==========
 
-## Notes
+壁纸来源：http://wallpaperscraft.com/
 
-## How it works
+### 选项
 
-1. It crawls the URL of those wallpapers and store them into the database.
-2. Download wallpapers using URLS from database and store them to target directory.
+```
+Usage      : python ./Wallpaper_DB.py resolution threshold mode [--proxy]
+             python ./Wallpaper_DB.py resolution --logfile
+	     python ./Wallpaper_DB.py -h
 
-### Some variables to customize
+resolution : 1366x768, 1920x1080, 360x640, 1024x768, 1600x900, 1280x900, 1440x900, 1280x1024, 800x600, 1680x1050, 2560x1440, 320x480, 1920x1200, 480x800, 720x1280. Please refer to http://wallpaperscraft.com/ for  more supported resolution.
 
-- categories: Those categories you are interested in. It includes all by default.
-- threshold: The number of wallpapers to download in each category.
-- resolution: The resultion of you computer.
-- path: The directory to store all of the folds of each category.
+threshold  : It is a integer which represents the number of wallpapers you want to download for EACH category.
 
-### Database Configruation
+mode       : online or local. online option means the URLS of wallpapers are crawled from the Internet if necessary or read directory from the Database. local option means the URLS of wallpapers is read from local *.org. Note: The *.org files is generated using --logfile option.
 
-- Database name: wallpapers
-- Database user: repl
-- Database password: slackware
-- The only table in `wallpapers`: wallpaper
-- Table info
+--proxy    : If this option is given, the script will use socks5 proxy when downloading wallpapers using wget and proxychains. This option is optional.
 
-![Table info](http://wstaw.org/m/2017/03/17/plasma-desktopJn1799.png)
+--logfile : This option will export all of the wallpaper URLs of each category from the database to .org files and save those files into the current directory named `urls`. It assumes your database is not empty.
+
+*Note*: Please update the `categories` variable if necessary. This script will download all of the categories by default.
+
+eg.
+python Wallpaper_DB.py 1440x900 2 online --proxy
+python Wallpaper_DB.py 1440x900 2 online
+python Wallpaper_DB.py 1440x900 2 local --proxy
+python Wallpaper_DB.py 1440x900 2 local
+python Wallpaper_DB.py 1440x900 --logfile
+
+```
+
+### 说明
+
+1. 在`__main__`里根据自己的需要更改`categories`来指定自己想要爬取的壁纸类别。
+2. 数据库使用MySQL，数据库名称：`wallpapers`，用户名：`repl`，数据库密码：`slackware`，根据自己的需要在程序里面更改。
+3. 目录`urls`里面已经包含了我爬取的分辨率为`1366x768`的总共10704张壁纸的地址，可以直接使用，在不需要数据库的情况下可以直接下载壁纸（每个类别下载2张）：
+
+```
+python Wallpaper_DB.py 1440x900 2 local
+```
+
+或者使用代理：
+
+```
+python Wallpaper_DB.py 1440x900 2 local --proxy
+```
+
+4. 依赖：`peewee`，`BeautifulSoup`，`requests`，`wget`。如果需要使用代理，需要`proxychains4`。
 
 ![Wallpaper folder](http://wstaw.org/m/2017/03/17/plasma-desktopqj1799.png)
--- Wallpaper_DB.py
+++ Wallpaper_DB.py
@@ -2,88 +2,124 @@
 
 # coding = utf-8
 
-# 2017/03/13 17:22:26
+# Created: 2017/03/13 17:22:26
+# Implement basic function
+
+# Updated: 2017/03/29 15:18:43
+# Fix some bugs. Optimize and refactor code.
 
 import peewee
-from peewee import *
 from bs4 import BeautifulSoup
 import requests
-import urllib
 import os
 import os.path
+import sys
 import subprocess
-import re
 
 
 class Wallpaper(peewee.Model):
     id = peewee.PrimaryKeyField()
-    url = peewee.CharField(unique = True)
-    pageurl = peewee.CharField(default = "")
+    url = peewee.CharField(unique=True)
+    # pageurl is the webpage which contains the picture
+    pageurl = peewee.CharField(default="")
     filename = peewee.CharField()
-    filesize = peewee.IntegerField()
+    filesize = peewee.IntegerField(default=0)
     category = peewee.CharField()
     pop_resolution = peewee.CharField()
     downloaded = peewee.BooleanField()
 
     class Meta:
-        database = MySQLDatabase('wallpapers', user = 'repl', passwd = 'slackware')
+        database = peewee.MySQLDatabase(
+            'wallpapers', user='repl', passwd='slackware')
 
 
-def handle_exception(url, proxy):
+def handle_exception(url, proxy=None):
     try:
-        rr = requests.get(url, proxies = proxy)
+        if proxy:
+            rr = requests.get(url, proxies=proxy)
+        else:
+            rr = requests.get(url)
         return rr
     except:
         return None
 
 
-def process_div(div, proxy, all_urls, cnt, all_pageurls, category):
+# TODO: all_pageurls and all_urls??
+def process_div(div, proxy, all_urls, cnt, category, resolution):
     url = 'http:' + div.find('a').get('href')
-    if url in all_pageurls: return None
+
+    if url in all_urls:
+        return None, None
+
     rr = handle_exception(url, proxy)
-    if not rr: return None
+
+    if not rr:
+        return None, None
+
     ss = BeautifulSoup(rr.content, 'html5lib')
     a_tag = ss.find('a', class_='wd_zoom')
 
     if a_tag is not None:
         img_url = a_tag.img.get('src')
         img_url = 'http:' + img_url
-        wallpaper, created = Wallpaper.get_or_create(url = img_url, filename = os.path.basename(img_url), category = category, pop_resolution = '1366x768', downloaded = False, pageurl = url)
+        wallpaper, created = Wallpaper.get_or_create(
+            url=img_url,
+            filename=os.path.basename(img_url),
+            category=category,
+            pop_resolution=resolution,
+            downloaded=False,
+            pageurl=url)
+        cnt += 1
+        print category, cnt, img_url
         if created:
-            cnt += 1
             wallpaper.save()
-            print category, cnt, img_url
             all_urls.append(img_url)
 
-    return all_urls
+    return all_urls, cnt
 
 
-def iterate_pages(pages_num, cnt, threshold, main_url, proxy, category, all_urls):
-    all_pageurls = [wall.pageurl for wall in Wallpaper.select().where(Wallpaper.category == category, Wallpaper.pageurl != "")]
-
+def iterate_pages(pages_num, cnt, threshold, main_url, proxy, category,
+                  all_urls, resolution):
     for page in range(pages_num):
-        if cnt >= threshold or Wallpaper.select().where(Wallpaper.category == category).count() >= threshold: break
+        if cnt >= threshold or Wallpaper.select().where(
+                Wallpaper.category == category,
+                Wallpaper.pop_resolution == resolution).count() >= threshold:
+            break
         p = page + 1
         url = main_url
         url = url if p == 1 else url + '/page' + str(p)
         r = handle_exception(url, proxy)
-        if not r: continue
+        if not r:
+            continue
         soup = BeautifulSoup(r.content, 'html5lib')
         divs = soup.find('div', class_='wallpapers')
 
         for div in divs:
-            _all_urls = process_div(div, proxy, all_urls, cnt, all_pageurls, category)
+            if cnt >= threshold:
+                break
+            cnt_ = cnt
+            _all_urls, cnt = process_div(div, proxy, all_urls, cnt, category,
+                                         resolution)
+            cnt = cnt_ if not cnt else cnt
             all_urls = _all_urls if _all_urls else all_urls
 
 
-# craw a category
-def crawl_urls(category, threshold, resolution):
-    proxy = { 'socks5' : 'socks5://127.0.0.1:1080',
-          'http' : 'socks5://127.0.0.1:1080',
-          'https' : 'socks5://127.0.0.1:1080'}
-    main_url = 'http://wallpaperscraft.com/catalog/' + category + '/' + resolution
+# crawl a category
+def crawl_urls(category, threshold, resolution, use_proxy):
+    if use_proxy:
+        proxy = {
+            'socks5': 'socks5://127.0.0.1:1080',
+            'http': 'socks5://127.0.0.1:1080',
+            'https': 'socks5://127.0.0.1:1080'
+        }
+    else:
+        proxy = None
+
+    main_url = 'http://wallpaperscraft.com/catalog/' + category \
+               + '/' + resolution
 
     r = handle_exception(main_url, proxy)
+
     if not r:
         print "Error to get category: ", category
         return
@@ -91,64 +127,53 @@ def crawl_urls(category, threshold, resolution):
     soup = BeautifulSoup(r.content, 'html5lib')
     pages = soup.find('div', class_='pages')
     pages_num = int(pages.select("a:nth-of-type(5)")[0].string)
-    all_pageurls = [wall.pageurl for wall in Wallpaper.select().where(Wallpaper.category == category)]
+    all_pageurls = [
+        wall.pageurl
+        for wall in Wallpaper.select()
+        .where(Wallpaper.category == category, Wallpaper.pop_resolution ==
+               resolution)
+    ]
     cnt = len(all_pageurls)
-    iterate_pages(pages_num, cnt, threshold, main_url, proxy, category, all_pageurls)
+    iterate_pages(pages_num, cnt, threshold, main_url, proxy, category,
+                  all_pageurls, resolution)
 
 
-def crawl(categories, threshold, resolution):
+def crawl(categories, threshold, resolution, use_proxy):
     for category in categories:
-        print "------", category
-        if Wallpaper.select().where(Wallpaper.category == category).count() >= threshold:
+        print '-' * 20, '{:^12}'.format(category), '-' * 20
+        if Wallpaper.select().where(
+                Wallpaper.category == category,
+                Wallpaper.pop_resolution == resolution).count() >= threshold:
             continue
-        crawl_urls(category, threshold, resolution)
+        crawl_urls(category, threshold, resolution, use_proxy)
 
 
-def filename2pageurl(filename):
-    url = 'http://wallpaperscraft.com/download/'
-    filename = re.split('\.', filename)[0]
-    resolution = re.split('_', filename)[-1]
-    path = '_'.join(re.split('_', filename)[:-1])
-    return url + path + '/' + resolution
+def download_1image(img_url,
+                    img_name,
+                    cnt,
+                    use_proxy,
+                    category,
+                    from_db,
+                    pic=None):
+    print "\n" + "*" * 20 + "{:^15}".format("Downloading") + "*" * 20
+    str_ = "category = {0}\nimg_url = {1}\nimg_name = {2}\ncnt = {3}\n"
+    print str_.format(category, img_url, img_name, cnt + 1)
 
+    if use_proxy:
+        cmd = ['proxychains4', 'wget', img_url, '-O', img_name]
+    else:
+        cmd = ['wget', img_url, '-O', img_name]
 
-def insert_page_url():
-    for pic in Wallpaper.select().where(Wallpaper.pageurl == ""):
-        pic.pageurl = filename2pageurl(pic.filename)
-        pic.save()
-        print pic.id, pic.pageurl
-
-
-def check_page_url():
-    resolution = '1366x768'
-
-    for pic in Wallpaper.select().where(Wallpaper.pageurl != ""):
-        if not pic.pageurl.endswith(resolution):
-            print pic.id, pic.pageurl
-
-
-
-def download_1image(img_url, img_name, pic, cnt):
-    # cmd = ['proxychains4', 'wget', img_url, '-O', img_name]
-    cmd = ['wget', img_url, '-O', img_name]
-    # subprocess.Popen(cmd).wait()
-    re = subprocess.call(cmd)
-    
-    # cmd_str = 'wget ' + img_url + ' -O ' + img_name
-    # os.system(cmd_str)
-        
-    # f = open(img_name, 'wb')
-    # f.write(requests.get(img_url).content)
-    # f.close()
-
+    res = subprocess.call(cmd)
     file_size = os.stat(img_name).st_size
-    pic.filesize = file_size
+    if from_db:
+        pic.filesize = file_size
 
-    print "\n" + "*" * 10 + "\n" + "file_size = {0}\nIn Function, cnt = {1}\nre = {2}\n".format(file_size, cnt, re) + "\n" + "*" * 10 + "\n"
-
-    if re == 0 and file_size >= 100000:
-        pic.downloaded = True
-        pic.save()
+    # remove files whose size is below 90000. It's possibly corrupted.
+    if res == 0 and file_size >= 90000:
+        if from_db:
+            pic.downloaded = True
+            pic.save()
         cnt += 1
         return cnt
     else:
@@ -156,39 +181,185 @@ def download_1image(img_url, img_name, pic, cnt):
         return None
 
 
-def download_images(path, categories, threshold):
+def download_from_db(category, resolution, cnt, threshold, dir_path,
+                     use_proxy):
+    for pic in Wallpaper.select().where(
+            Wallpaper.category == category,
+            Wallpaper.pop_resolution == resolution).order_by(Wallpaper.id):
+        if cnt >= threshold:
+            break
+        if pic.downloaded:
+            cnt += 1
+            print "Already downloaded: {0}, category: {1}\n".format(
+                cnt, category)
+            continue
+
+        _cnt = download_1image(pic.url,
+                               os.path.join(dir_path, pic.filename), cnt,
+                               use_proxy, category, True, pic)
+        cnt = _cnt if _cnt else cnt
+
+
+def download_from_org(category, resolution, cnt, threshold, dir_path,
+                      use_proxy):
+    path = os.path.join(os.getcwd(), "urls", resolution)
+
+    if not os.path.exists(path):
+        os.exit("Directory not found: " + path)
+
+    file_name = os.path.join(path, category + ".org")
+
+    if os.path.exists(file_name):
+        with open(file_name, "r") as f:
+            for line in f:
+                if cnt >= threshold:
+                    break
+                url = line.rstrip()
+                _cnt = download_1image(url,
+                                       os.path.join(dir_path,
+                                                    os.path.basename(url)),
+                                       cnt, use_proxy, category, False)
+                cnt = _cnt if _cnt else cnt
+
+
+def download_images(path, categories, threshold, resolution, use_proxy,
+                    from_db):
     for category in categories:
         cnt = 0
         dir_path = os.path.join(path, category)
+
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-        for pic in Wallpaper.select().where(Wallpaper.category == category).order_by(Wallpaper.id):
-            if cnt >= threshold: break
-            if pic.downloaded:
-                cnt += 1
-                print "Already downloaded: {0}, category: {1}\n".format(cnt, category)
-                continue
-            
-            img_url = pic.url
-            img_name = os.path.join(dir_path, pic.filename)
+        if from_db:
+            download_from_db(category, resolution, cnt, threshold, dir_path,
+                             use_proxy)
+        else:
+            download_from_org(category, resolution, cnt, threshold, dir_path,
+                              use_proxy)
 
-            print "\n" + "*" * 10 + "\n" + "category = {0}\nimg_url = {1}\nimg_name = {2}\ncnt = {3}\n".format(category, img_url, img_name, cnt) + "*" * 10 + "\n"
-            # i__ = raw_input()
 
-            _cnt = download_1image(img_url, img_name, pic, cnt)
-            cnt = _cnt if _cnt else cnt
+def crawl_main(categories, resolution, pictures_dir, threshold, proxyp):
+    path = os.path.join(pictures_dir, resolution)
+
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    crawl(categories, threshold, resolution, proxyp)
+    download_images(path, categories, threshold, resolution, proxyp, True)
+
+
+# export all picture urls to .org text file
+def log2file(resolution, all_categories):
+    path = os.path.join(os.getcwd(), "urls", resolution)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for category in all_categories:
+        file_name = os.path.join(path, category + ".org")
+        urls = [
+            wall.url
+            for wall in Wallpaper.select()
+            .where(Wallpaper.category == category, Wallpaper.pop_resolution ==
+                   resolution)
+        ]
+        exist_urls = []
+
+        if os.path.exists(file_name):
+            with open(file_name, "r") as f:
+                for line in f:
+                    exist_urls.append(line.rstrip())
+
+        print category
+
+        with open(file_name, "a+") as f:
+            for url in urls:
+                if url not in exist_urls:
+                    print "Add url: ", url
+                    f.write(url + "\n")
+
+
+def check_argv(categories, all_categories, pop_resolutions):
+    pop_resolutions_str = ', '.join(
+        pop_resolutions) + ". Please refer to http://wallpaperscraft.com/ for \
+ more supported resolution."
+
+    pictures_dir = os.path.expanduser("~/Pictures/Wallpapers")
+
+    if not os.path.exists(pictures_dir):
+        sys.exit("ERROR: directory '{0}' not found".format(pictures_dir))
+
+    if len(sys.argv) == 2 and sys.argv[1] == "-h":
+        print '''Usage      : python ./Wallpaper_DB.py resolution threshold mode [--proxy]
+             python ./Wallpaper_DB.py resolution --logfile
+
+resolution : {0}
+
+threshold  : It is a integer which represents the number of wallpapers you \
+want to download for EACH category.
+
+mode       : online or local. online option means the URLS of wallpapers are \
+crawled from the Internet if necessary or read directory from the \
+Database. local option means the URLS of wallpapers is read from local \
+*.org. Note: The *.org files is generated using --logfile option.
+
+--proxy    : If this option is given, the script will use socks5 proxy \
+when downloading wallpapers using wget and proxychains. This option is \
+optional.
+
+--logfile : This option will export all of the wallpaper URLs of each \
+category from the database to .org files and save those files into \
+the current directory named `urls`. It assumes your database is not empty.
+
+*Note*: Please update the `categories` variable if necessary. This \
+script will download all of the categories by default.
+
+eg.
+python Wallpaper_DB.py 1440x900 2 online --proxy
+python Wallpaper_DB.py 1440x900 2 online
+python Wallpaper_DB.py 1440x900 2 local --proxy
+python Wallpaper_DB.py 1440x900 2 local
+python Wallpaper_DB.py 1440x900 --logfile'''.format(pop_resolutions_str)
+        sys.exit()
+
+    if len(sys.argv) < 3:
+        sys.exit(
+            "Usage: python ./Wallpaper_DB.py resolution threshold [-proxy]\n" +
+            "Popular resolutions are: " + pop_resolutions_str)
+
+    resolution = sys.argv[1]
+
+    if len(sys.argv) == 3:
+        if (sys.argv[2] == "--logfile"):
+            log2file(resolution, all_categories)
+    elif len(sys.argv) >= 4:
+        proxyp, threshold = None, int(sys.argv[2])
+
+        if len(sys.argv) > 4 and sys.argv[4] == "--proxy":
+            proxyp = True
+
+        if sys.argv[3] == "online":
+            crawl_main(categories, resolution, pictures_dir, threshold, proxyp)
+        elif sys.argv[3] == "local":
+            download_images(
+                os.path.join(pictures_dir, resolution), categories, threshold,
+                resolution, proxyp, False)
 
 
 if __name__ == '__main__':
-    Wallpaper.create_table(fail_silently = True)
+    Wallpaper.create_table(fail_silently=True)
 
-    categories = ['3d', 'anime', 'abstract', 'animals', 'city', 'fantasy', 'flowers', 'food', 'games', 'girls', 'hi-tech', 'holidays', 'macro', 'men', 'movies', 'music', 'nature', 'other', 'space', 'sport', 'textures', 'tv-series', 'vector']
-    threshold = 100
-    resolution = '1366x768'
-    path = '/home/repl/Pictures/Wallpapers/All'
+    pop_resolutions = [
+        '1366x768', '1920x1080', '360x640', '1024x768', '1600x900', '1280x900',
+        '1440x900', '1280x1024', '800x600', '1680x1050', '2560x1440',
+        '320x480', '1920x1200', '480x800', '720x1280'
+    ]
 
-    # insert_page_url()
-    # check_page_url()
-    crawl(categories, threshold, resolution)
-    download_images(path, categories, threshold)
+    all_categories = [
+        '3d', 'anime', 'abstract', 'animals', 'city', 'fantasy', 'flowers',
+        'food', 'games', 'girls', 'hi-tech', 'holidays', 'macro', 'men',
+        'movies', 'music', 'nature', 'other', 'space', 'sport', 'textures',
+        'tv-series', 'vector'
+    ]
+    # categories = ['games', 'hi-tech']
+    categories = all_categories
+    check_argv(categories, all_categories, pop_resolutions)
